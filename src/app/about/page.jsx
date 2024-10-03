33b02f4d7ed419b61b0b148708e11d8ebbbfb9c1
ademola olukotun <icuteade@gmail.com>
2024-10-03 10:02:50 -0700

    navbar component

---
+++ src/app/about/page.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4 p-3'>
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia,
+        molestiae? Incidunt quas possimus similique maiores, nostrum voluptates
+        nihil adipisci. Beatae suscipit ratione repellendus libero! Adipisci
+        dolorem, aliquid est, iusto dolor, perspiciatis consectetur aperiam
+        dolores sunt nesciunt accusamus nulla sint magni! Consequatur eum ea
+        ipsa, repellendus sunt fugiat beatae odio vel.
+      </p>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia,
+        molestiae? Incidunt quas possimus similique maiores, nostrepellendus libero! Adipisci
+        dolorem, aliquid est, iusto dolor, perspiciatis consectetur aperiam
+        dolores sunt nesciunt accusamus nulla sint magni! Consequatur eum ea
+        ipsa, repellendus sunt fugiat beatae odio vel.
+      </p>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia,
+        molestiae? Incidunt quas possimus similique maiores, nostrum voluptates
+        nihil adipisci. Beatae suscipit ratione repellendus libero! Adipisci
+        dolorem, apiciatis consectetur aperiam
+        dolores sunt nesciunt accusamus nulla sint magni! Consequatur eum ea
+        ipsa, repellendus sunt fugiat beatae odio vel.
+      </p>
+    </div>
+  );
+}
